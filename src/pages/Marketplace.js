42c220bf5ecf280724f1242aad968e6a7fd0306c
sihui2709 <Hazel3466!>
2024-12-02 13:59:41 +0800

    added in image folders and display image on marketplace

--- src/pages/Marketplace.js
+++ src/pages/Marketplace.js
@@ -18,11 +18,11 @@ const Marketplace = () => {
         </div>
         <div className="marketplace-listings">
             {[
-              { price: '1.40 ETH', title: 'Your Brain social media addiction' },
-              { price: '1.50 ETH', title: 'Land damaged by me and nature' },
-              { price: '1.32 ETH', title: 'Aurora Wave Dark Purple Magma Tone' },
-              { price: '1.86 ETH', title: "Ancient coins from China's past" },
-              { price: '1.93 ETH', title: 'Yellow sea coral preserved by me' },
+              { image:'/cardImages/0.png', price: '1.40 ETH', title: 'Your Brain social media addiction' },
+              { image:'/cardImages/1.png', price: '1.50 ETH', title: 'Land damaged by me and nature' },
+              { image:'/cardImages/2.png', price: '1.32 ETH', title: 'Aurora Wave Dark Purple Magma Tone' },
+              { image:'/cardImages/3.png', price: '1.86 ETH', title: "Ancient coins from China's past" },
+              { image:'/cardImages/4.png', price: '1.93 ETH', title: 'Yellow sea coral preserved by me' },
               { price: '1.40 ETH', title: 'Your Brain social media addiction' },
               { price: '1.50 ETH', title: 'Land damaged by me and nature' },
               { price: '1.32 ETH', title: 'Aurora Wave Dark Purple Magma Tone' },
@@ -30,7 +30,7 @@ const Marketplace = () => {
               { price: '1.93 ETH', title: 'Yellow sea coral preserved by me' },
             ].map((listing, index) => (
               <div className="marketplace-card" key={index}>
-                <div className="card-image"></div>
+                <img src={listing.image} className="card-image" />
                 <p className="card-price">{listing.price}</p>
                 <p className="card-title">{listing.title}</p>
                 <button className="place-bid-btn">Place Your Bid</button>
