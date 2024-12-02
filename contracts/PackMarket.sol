2c7c99a1d46357c72642a9501b8524af1c59f1e1
tenghsuen <123951674+tenghsuen@users.noreply.github.com>
2024-12-03 02:13:50 +0800

    Packmarket smart contract
    ownable and safemath are placeholders for the actual dependencies

---
+++ contracts/PackMarket.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "./ownable.sol";
+import "./safemath.sol";
+
+
+contract PackMarket is Ownable{
+
+    using SafeMath for uint256;
+
+    uint256 public packPrice = 0.0001 ether;
+    uint256 public packsAvailable = 100;
+    uint256 public totalPacksSold = 0;
+
+    event PackPurchased(address indexed buyer, uint256 packId);
+
+    function purchasePack() external payable {
+        require(packsAvailable > 0, "All card packs are currently sold out");
+        require(msg.value == packPrice, "Incorrect payment amount");
+
+        totalPacksSold = totalPacksSold.add(1);
+
+        emit PackPurchased(msg.sender, totalPacksSold);
+    }
+
+    function withdraw() external onlyOwner {
+        address payable _owner = payable(owner());
+        _owner.transfer(address(this).balance);
+    }
+
+    function updatePacksAvailable(uint256 newQty) external onlyOwner {
+        // reset the number of packs available
+        packPrice = newQty;
+    }    
+    
+    function updatePackPrice(uint256 newPrice) external onlyOwner {
+        // newPrice in wei
+        packPrice = newPrice;
+    }
+}
